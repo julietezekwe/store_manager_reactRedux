3a36c4c27d4a3a1ba69b132b4a68878e4d4cdb73
chidimma Ezekwe <julietezekwe@gmail.com>
2019-02-13 15:06:02 +0100

    Chore(Setups) Setup react/redux project with webpack setup eslint setup babel setup webpack
    setup react/redux
    setup testing with jest/enzyme
    setup travis
    [Chore #62980053]

---
+++ webpack.common.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: ['babel-polyfill', './src/index.jsx'],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: 'index-bundle.js',
+    publicPath: '/'
+  },
+  node: {
+    fs: 'empty'
+  },
+  devServer: {
+    port: 8080,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+
+            sourceMap: true,
+            importLoaders: 1
+          },
+        },
+      },
+      {
+        test: /\.(ttf|eot)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: {
+          loader: 'file-loader',
+        },
+      },
+      {
+        test: /\.(png|svg|gif|jpg)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 25000,
+          },
+        },
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+  ]
+};
